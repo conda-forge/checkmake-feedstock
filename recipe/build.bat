555271cf989d1f9d0a0ec1e627cb09621d47728f
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-25 18:44:06 -0800

    use rattler

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,7 +1,7 @@
 @echo on
 @setlocal EnableDelayedExpansion
 
-go build -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s -X 'main.version=%PKG_VERSION%'" || goto :error
+go build -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s -X main.version=%PKG_VERSION%" || goto :error
 go-licenses save . --save_path=license-files || goto :error
 
 goto :eof
